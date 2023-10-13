3f5a365b8d65ff0b687b995976bf9fe53dd236b8
Ahsan Khan <ahsankhan1911@gmail.com>
2023-10-14 01:20:37 +0500

    [Config] added app level configs

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,18 @@
+import { router } from './app/routes';
 import express, { Application } from 'express';
+import cors from 'cors';
 
 export const app: Application = express();
+
+//Disable x-powered-by response header for appilcation security purpose
+app.disable('x-powered-by');
+
+//CORS config
+app.use(cors());
+
+//body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended : true }));
+
+//API urls binded
+app.use('/api', router);
