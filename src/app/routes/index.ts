1e0ca67f98a37c4f11f5e6be08fac665aba9904a
Ahsan Khan <ahsankhan1911@gmail.com>
2023-10-15 20:22:26 +0500

    [API] added app routes

---
+++ src/app/routes/index.ts
@@ -0,0 +1,23 @@
+
+import { Router } from 'express';
+import { createTask, deleteTask, getTaskById, getTasks, updateTask, registerUser, loginUser  } from 'app/controllers';
+import { authenticateUser, validateTask, validateUser } from 'app/middlewares';
+
+export const router = Router();
+
+// Tasks
+router.post('/task', authenticateUser,  validateTask, createTask);
+
+router.get('/task/:id', authenticateUser, getTaskById);
+
+router.put('/task/:id', authenticateUser, updateTask);
+
+router.delete('/task/:id', authenticateUser, deleteTask);
+
+router.get('/tasks', authenticateUser, getTasks);
+
+// Users
+router.post('/user/register', validateUser, registerUser);
+
+router.post('/user/login', loginUser);
+
