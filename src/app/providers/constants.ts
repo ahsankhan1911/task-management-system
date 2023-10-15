af7466fcb49865fc8337a75d8ecb9abd5e91ba9a
Ahsan Khan <ahsankhan1911@gmail.com>
2023-10-15 20:19:22 +0500

    [Providers] exported providers and constants

---
+++ src/app/providers/constants.ts
@@ -0,0 +1,10 @@
+export const MESSAGES = {
+  INTERNAL_SERVER_ERROR: 'Internal Server Error',
+  UNAUTHORIZED_ACCESS: 'Cant validate access. Please login',
+  ACCESS_FORBIDDEN: 'Access forbidden.',
+  VALIDATION_ERROR: 'Validation error.',
+  TOKEN_GENERATING_ERROR: 'Error while generating access token.',
+  INVALID_EMAIL: 'Please fill valid email address.',
+  DATA_NOT_FOUND: 'No data found.',
+  SOMETHING_WENT_WRONG: 'Something went wrong.',
+};
---
+++ src/app/providers/index.ts
@@ -0,0 +1,5 @@
+export * from './api';
+export * from './jwt';
+export * from './store';
+export * from './crypto';
+export * from './bcrypt';
